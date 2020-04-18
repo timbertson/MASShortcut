2f6ba59888cce9f64dc9d6858717923c998b66d2
Ram Patra <ramswarooppatra@gmail.com>
2020-04-18 13:33:09 +0100

    Added all headers to include folder

--- Package.swift
+++ Package.swift
@@ -1,15 +1,26 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "MASShortcut",
     products: [
-        .library(name: "Framework",
-                 targets: ["Framework"])
+        .library(name: "MASShortcut", targets: ["MASShortcut"])
     ],
     targets: [
-        .target(name: "Framework",
-                path: "Framework")
+        .target(
+            name: "MASShortcut",
+            path: "Framework",
+            exclude: [
+                "Model/MASShortcutTests.m",
+                "Monitoring/MASHotKeyTests.m",
+                "Monitoring/MASShortcutMonitorTests.m",
+                "User Defaults Storage/MASDictionaryTransformerTests.m",
+                "User Defaults Storage/MASShortcutBinderTests.m"
+            ],
+            cSettings: [
+                .headerSearchPath("UI"),
+            ]
+        )
     ]
 )
